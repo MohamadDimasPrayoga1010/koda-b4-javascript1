c80dc3f94d6584e7c9c0008383f69bb3c2e4cba0
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-03 17:43:38 +0700

    Update minitask2.js

--- minitask2.js
+++ minitask2.js
@@ -1,6 +1,5 @@
 //Buatkan perulangan segitiga siku siku
 
-
 for (let i = 0; i < 5; i++){
     let x = "";
     for(let y = 0; y < i; y++){
@@ -8,3 +7,22 @@ for (let i = 0; i < 5; i++){
     }
     console.log(x);
 }
+
+
+let i = 0;
+
+do {
+  let x = "";
+  let y = 0;
+
+  do {
+    x += "*"
+    y++
+  } while (y < i)
+
+  console.log(x)
+  i++;
+} while (i < 5)
+
+
+
